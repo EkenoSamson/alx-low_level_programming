e791d94f977bc27ae74137ac67b0684e9f74d4e1
EkenoSamson <samsonekeno98@gmail.com>
2023-02-16 08:51:44 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+  * main - A program that prints a staement using printf
+  * Return: 0 when successful
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
