6c38e714951ae09e0ee0308ea468bacecc40cff1
EkenoSamson <samsonekeno98@gmail.com>
2023-02-16 09:51:10 -0800

    fput

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+  * main - a pogram that uses putchar
+  * Return: 1 when successful
+  */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
