9503a3aa959e05d3320a8e7b33c2d7db64fb8a91
EkenoSamson <samsonekeno98@gmail.com>
2023-02-16 08:37:43 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description : "a program that outputs a statement using puts function"
+* Return value : 0 when successful.
+*/
+void main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
