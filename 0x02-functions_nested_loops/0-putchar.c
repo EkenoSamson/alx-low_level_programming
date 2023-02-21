6cea790ede57e091f03a0ac572d44675b7b71489
EkenoSamson <samsonekeno98@gmail.com>
2023-02-21 01:51:52 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - entry point
+  * Description: a program that prints _putchar, followed by a new line
+  * Return: 0 when successful
+  */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
