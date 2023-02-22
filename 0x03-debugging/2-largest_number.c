d3b8d2f3907a221ac7d03e790b9a0ab73ecd0521
EkenoSamson <samsonekeno98@gmail.com>
2023-02-22 01:08:43 -0800

    largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,14 +16,20 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
-	{
-		largest = b;
-	}
-	else
+	else if (a > b && c > a)
 	{
 		largest = c;
 	}
+	else if (b > a && c > a) 
+	{
+		if (b > c)
+			largest = b;
+		else
+			largest = c;
+	}
+	else if (b > a && a > c)
+		largest = b;
+
 
 	return (largest);
 }
