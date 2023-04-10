08bec0425eebb07663bd1ba46e225ba198a6a89e
EkenoSamson <samsonekeno98@gmail.com>
2023-04-10 16:43:31 -0700

    append to the file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -27,7 +27,7 @@ int append_text_to_file(const char *filename, char *text_content)
 			return (-1);
 	}
 
-	op = open(filename, O_WRONLY | O_APPEND);
+	op = open(filename, O_WRONLY | O_APPEND | O_CREAT | O_EXCL );
 
 	if (op == -1)
 	{
