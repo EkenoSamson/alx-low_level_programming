67b36f5d2725402a6b859a109e6d3e6ff4e76033
EkenoSamson <samsonekeno98@gmail.com>
2023-04-10 19:10:12 -0700

    ELF

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * check_elf - Checks if a file is an ELF file.
+ * print_version - the version.
  * @e_ident: A pointer to an array containing the ELF magic numbers.
  *
  * Description: If the file is not an ELF file - exit code 98.
