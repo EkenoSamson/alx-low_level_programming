5050deede84ba8917639aa0211db1cbb5e2cfa2d
EkenoSamson <samsonekeno98@gmail.com>
2023-04-11 06:11:56 -0700

    file I/O

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -18,8 +18,6 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void close_f(int fd);
 
-#include "main.h"
-
 void close_f(int fd)
 {
 	int z;
