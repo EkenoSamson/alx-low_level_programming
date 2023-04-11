93489aadd9ac37f18576b9ece76ac658e5839ad2
EkenoSamson <samsonekeno98@gmail.com>
2023-04-10 17:15:36 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,20 +17,25 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
-		op = open(filename, O_CREAT, 0600);
-	else
-		op = open(filename, O_RDONLY | O_TRUNC | O_CREAT, 0600);
+	if (text_content != NULL)
+	{
+		while(text_content[content])
+			content++;
+	}
+	op = open(filename, O_CREAT |O_RDWR | O_TRUNC, 0600);
 
 	if (op == -1)
+	{
+		close(op);
 		return (-1);
-
-	while (text_content[content])
-		content++;
+	}
 	wr = write(op, text_content, content);
 
-	if (wr == -1)
+	if (wr == -1 || wr != content)
+	{
+		close(op);
 		return (-1);
+	}
 	close(op);
 	return (1);
 }
