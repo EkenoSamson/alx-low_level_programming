a831e8bca4a7960df2d18e62566dbafd9d1453e1
EkenoSamson <samsonekeno@gmail.com>
2023-08-02 13:58:04 -0700

    palindromic nature

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,6 +11,7 @@ int _sqrt_recursion(int n);
 int _sqrt(int s, int r);
 int is_prime_number(int n);
 int is_palindrome(char *s);
+int _palindrome(char *s, int len);
 int wildcmp(char *s1, char *s2);
 
 #endif
