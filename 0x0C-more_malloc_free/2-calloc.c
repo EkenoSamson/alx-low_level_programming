d0645cc4b6c7ef3205ee65fac03174acd96ecf0e
EkenoSamson <samsonekeno@gmail.com>
2023-08-10 12:22:19 -0700

    created calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,33 +1,45 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
-  * _calloc - allocates memory of an array using malloc.
-  *
-  * @nmemb: number of elements in array.
-  * @size: size of elements of array.
-  *
-  * Return: NULL is size or nmemb == 0.
-  * NULL if malloc fails.
-  * Pointer to memory allocated if successful.
-  */
+ * _calloc - Function that allocates memory for an an array and intialize to 0.
+ *
+ * @nmemb: no of blocks.
+ * @size: size of each block.
+ *
+ * Return: a pointer to the memory.
+ */
+
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *p;
-	unsigned int i;
+	void *ptr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	p = malloc(nmemb * size);
-	if (p == NULL)
-	{
+	ptr = malloc(nmemb * size);
+
+	if (ptr == NULL)
 		return (NULL);
-	}
 
-	for (i = 0; i < (nmemb * size); i++)
+	_initmem(ptr, nmemb * size);
+
+	return (ptr);
+}
+
+/**
+ * _initmem - Funtion that initialize a memory to zero.
+ * @s: pointer to character.
+ * @n: number of memories.
+ * Return: pointer.
+ */
+
+char *_initmem(char *s, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
 	{
-		*((char *)(p) + i) = 0;
+		s[i] = 0;
+		i++;
 	}
-
-	return (p);
+	return (s);
 }
