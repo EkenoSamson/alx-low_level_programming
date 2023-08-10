d742bc79d9dfddc9f5758e0a5c85b22bfe72507d
EkenoSamson <samsonekeno@gmail.com>
2023-08-10 13:48:18 -0700

    updated realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,7 +17,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 	if (ptr == NULL)
+	{
 		ptr_r = malloc(new_size);
+		if (ptr_r == NULL)
+			return (NULL);
+		return (ptr_r);
+	}
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
