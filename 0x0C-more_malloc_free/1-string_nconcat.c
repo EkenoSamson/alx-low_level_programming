b3d8fc46e488e06b2e8a6fa6937f35393507d2ed
EkenoSamson <samsonekeno@gmail.com>
2023-08-10 11:30:25 -0700

    added string_nconcat.h

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,59 +1,65 @@
 #include "main.h"
-#include <stdlib.h>
-
-int _strlen(char *string);
 
 /**
- * string_nconcat - a function that concatenates two strings.
+ * _strlen - function to return lenght of a string.
  *
- * @s1: string to append to
- * @s2: string to  append
- * @n: number of bytes
+ * @s: string to be measured.
+ * Return: length of the string.
+ */
+
+int _strlen(char *s)
+{
+	unsigned int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	return (i);
+}
+
+/**
+ * string_nconcat - Function that adds n characters of str2 to str1.
  *
- * Return: the pointer to the character
+ * @s1: string 1.
+ * @s2: string 2.
+ * @n: number of character from str2.
+ * Return: the pointer to string 1.
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
-	int num, len, i, j;
+	int size, i = 0, k = 0, n_char;
 
-	num = n;
-
-	if (s1 == NULL) /* account for NULL strings */
+	n_char = n;
+	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	if (num < 0) /* account for negative n bytes */
+	if (n_char < 0)
 		return (NULL);
-	if (num >= _strlen(s2)) /* account for n too big */
-		num = _strlen(s2);
+	if (n_char >= _strlen(s2))
+		n_char = _strlen(s2);
 
-	len = _strlen(s1) + num + 1; /* +1 to account for null pointer */
+	size = _strlen(s1) + n_char + 1;
+	ptr = malloc(size * sizeof(*ptr));
 
-	ptr = malloc(sizeof(*ptr) * len); /* malloc and check for error */
 	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0; s1[i] != '\0'; i++) /* concat */
+	while (s1[i] != '\0')
+	{
 		ptr[i] = s1[i];
-	for (j = 0; j < num; j++)
-		ptr[i + j] = s2[j];
-	ptr[i + j] = '\0';
+		i++;
+	}
+
+	while (n_char > k)
+	{
+		ptr[i + k] = s2[k];
+		k++;
+	}
 
 	return (ptr);
 }
 
-/**
- * _strlen - calculate and return string length
- * @string: string
- * Return: string length
- */
-int _strlen(char *string)
-{
-	int i;
 
-	for (i = 0; string[i] != '\0'; i++)
-		;
-	return (i);
-}
