c9cd240e1aa160c6a5bc803ae6460e39d646b31d
EkenoSamson <samsonekeno@gmail.com>
2023-08-10 12:21:41 -0700

    updated main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <string.h>
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
@@ -12,5 +13,6 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _strlen(char *s);
+char *_initmem(char *s, unsigned int n);
 
 #endif
