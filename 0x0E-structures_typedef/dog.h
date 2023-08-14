c9f133df680c08c079da050ddfff0cefcdcdfef5
EkenoSamson <samsonekeno@gmail.com>
2023-08-14 12:22:36 -0700

    created struct dog type

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * struct dog - a dog's basic info
- * @name: First member
- * @age: Second member
- * @owner: Third member
+ * struct dog - user defined data type to store dog information.
  *
- * Description: Longer description
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: owner of the dog.
+ *
+ * Description: use of structure to create dat type struct dog.
  */
 
 struct dog
@@ -17,13 +20,15 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - Typedef for struct dog.
+ */
+
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
 
 #endif
