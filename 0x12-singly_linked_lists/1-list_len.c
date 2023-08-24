0eaeb8ecef992abe841cd294f7ccb7ca5c898d68
EkenoSamson <samsonekeno98@gmail.com>
2023-08-24 08:28:13 -0700

    Created the repo, header file, 0-print_list and 1-list_count

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,21 +1,22 @@
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * list_len - returns the number of elements in a linked list
- * @h: pointer to the list_t list
- *
- * Return: number of elements in h
+ * list_len - count the number of elements a list.
+ * @h: pointer to the first element.
+ * Return: number of elements.
  */
 
 size_t list_len(const list_t *h)
 {
-	size_t n = 0;
+	size_t count = 0;
 
-	while (h)
+	if (h == NULL)
+		return (count);
+
+	while (h != NULL)
 	{
-		n++;
+		count++;
 		h = h->next;
 	}
-	return (n);
+	return (count);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,13 +1,17 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LISTS_H_
+#define _LISTS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
+ * struct list_s - singly linked lists.
+ * @str: string (malloc'ed string)
+ * @len: length of the string.
+ * @next: pointerto the next node.
  *
- * Description: singly linked list node structure
+ * Description: singly linked lists node structure.
  */
 
 typedef struct list_s
@@ -17,11 +21,11 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif
-
+#endif /* _LISTS_H_ */
